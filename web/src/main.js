9474b91e7c376a41315948a2371b36b2fb9a0b77
Raffaele Filiberti <raffaele@activetheory.net>
2025-03-04 10:20:09 +0100

    refactor: clean up main entrypoint

--- web/src/main.js
+++ web/src/main.js
@@ -1,17 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import RollAndScoreApp from './App.vue'
-import { getQueryParam } from './utils/get-query-param'
 import { createPinia } from 'pinia'
 
 const pinia = createPinia()
 
-let app
-if (getQueryParam('roolAndScore')) {
-  app = createApp(RollAndScoreApp)
-} else {
-  app = createApp(App)
-}
-
-app.use(pinia)
-app.mount('#app')
+createApp(App)
+  .use(pinia)
+  .mount('#app')
