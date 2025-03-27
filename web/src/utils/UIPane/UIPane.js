7084e2238655e073d030c2fe2b15383b8a707645
Thu Ya Kyaw <contact.thuya@gmail.com>
2025-03-27 20:50:49 +0900

    chore: clean up web folder

--- web/src/utils/UIPane/UIPane.js
+++
@@ -1,8 +0,0 @@
-import UIPaneBase from './UIPaneBase'
-
-export default class UIPane extends UIPaneBase {
-  constructor(identifier, jsonPath = null, props = null) {
-    super(identifier, jsonPath)
-    this.props = props
-  }
-}
